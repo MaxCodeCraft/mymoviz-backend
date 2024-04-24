13f12639e20c4a34b8000fb84e255a624c12ef2e
MaxCodeCraft <maxcodecraft06@gmail.com>
2024-04-24 11:14:31 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.AUTHENTIFICATION_KEY}`,
+  },
+};
+
+const urlRecommendations = "https://api.themoviedb.org/3/discover/movie";
+
+router.get("/movies", (req, res) => {
+  fetch(urlRecommendations, options)
+    .then((response) => response.json())
+    .then((data) => res.json({ movies: data.results }))
+    .catch((err) => console.error(err));
+});
+
+module.exports = router;
